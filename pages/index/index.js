ff2d49815ade9533c69717f65cbda01bbb43d377
liuhao1088 <1350569467@qq.com>
2021-01-26 17:18:02 +0800

    “logs”

---
+++ pages/index/index.js
@@ -0,0 +1,77 @@
+// pages/video/video.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   
+  },
+  toApplets(){
+     wx.navigateToMiniProgram({
+      appId: 'wxf63dcaf8f95ea541',
+      path:  "pages/home/home", //跳转小程序的路径
+      success(res) {
+        // 打开成功
+        console.log(res)
+      } 
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  // onShareAppMessage: function () {
+
+  // }
+})
